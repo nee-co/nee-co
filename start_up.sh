6b8ae4a1b894aa9d0f5dc1326e51c3a92672ead5
Tomohiko Mimura <mito.5525@gmail.com>
2016-07-30 02:16:29 +0900

    aldeaを追加する

--- start_up.sh
+++ start_up.sh
@@ -1,7 +1,20 @@
+# 事前準備
+# - Dockerが稼働する環境を用意する(Docker for Mac 1.12.0で動作確認済み)
+# - 各サブシステムでイメージを作成しておく( `aldea-application`  )
+# - jq をインストールしておく (必須ではないが、JSONをそのまま見るのは辛いので推奨)
+
 # 各DBを立ち上げる
-docker-compose up -d kong-database cuenta-database
+docker-compose up -d kong-database cuenta-database aldea-database
 
 # DBが立ち上がりきっていないとアプリが起動できないため少し待つ
+
+# Kong起動
+docker-compose up -d kong-application
+
+# Kongに登録されているAPI一覧を表示 => 空を確認
+curl -sS http://localhost:8001/apis | jq
+
+# Cuenta ============================================================
 
 # CuentaDB作成 初回のみ実行する
 docker-compose run cuenta-application mix ecto.setup
@@ -12,14 +25,8 @@ mysql -uroot -proot -h127.0.0.1 -P13306 cuenta_prod < dummy.sql
 # Cuenta起動
 docker-compose up -d cuenta-application
 
-# 試しにユーザ検索APIを読ぶ => ポートを開けていないため呼べないことを確認
+# 試しにユーザAPIを読ぶ => ポートを開けていないため呼べないことを確認
 curl http://localhost:4000/users/list?user_ids=1
-
-# Kong起動
-docker-compose up -d kong-application
-
-# Kongに登録されているAPI一覧を表示 => 空を確認
-curl -sS http://localhost:8001/apis | jq
 
 # KongにCuentaを登録
 curl -sS -X POST --url http://localhost:8001/apis/ --data 'name=cuenta' --data 'upstream_url=http://cuenta-application:4000' --data 'request_path=/users' | jq
@@ -29,3 +36,23 @@ curl -sS http://localhost:8001/apis | jq
 
 # Kong経由でユーザAPIを呼ぶ
 curl -sS http://localhost:8000/users/list?user_ids=1 | jq
+
+# Aldea ============================================================
+
+# AldeaDB作成 初回のみ実行する
+docker-compose run aldea-application bundle exec rails db:setup
+
+# Aldea起動
+docker-compose up -d aldea-application
+
+# 試しにイベントAPIを読ぶ => ポートを開けていないため呼べないことを確認
+curl http://localhost:3000/events
+
+# KongにAldeaを登録
+curl -sS -X POST --url http://localhost:8001/apis/ --data 'name=aldea' --data 'upstream_url=http://aldea-application:3000' --data 'request_path=/events' | jq
+
+# KongにAldeaが登録されていることを確認
+curl -sS http://localhost:8001/apis | jq
+
+# Kong経由でイベントAPIを呼ぶ
+curl -sS http://localhost:8000/events | jq
